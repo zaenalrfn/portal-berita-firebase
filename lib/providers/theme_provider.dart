d9faf89dfdc7420d2eacec62b8e6d2aa996827cc
zaenalrfn <ZAOfficial24@gmail.com>
2025-12-07 22:35:54 +0700

    feat: fix darkmode and done

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode _themeMode = ThemeMode.light;
 
   ThemeMode get themeMode => _themeMode;
 
